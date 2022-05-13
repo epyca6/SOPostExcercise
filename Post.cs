04f4ab9a1cd9d01202cd763888b6232d787b95f9
epyca6 <epyca6@gmail.com>
2022-05-13 16:30:55 +0200

    Initial version

---
+++ Post.cs
@@ -0,0 +1,73 @@
+﻿namespace StackOverflowExcersise
+{ 
+    class Post
+    {
+        // Property field
+        // All private fields makes sense if we are collecting the Posts in some key, value list 
+        private int _voteNumber { get; set; }
+        private string _title { get; }
+        private string _description { get; set; } = "";
+        public DateTime creationDate { get; }
+        private DateTime _updatedDate;
+
+        public DateTime UpdateDate
+        {
+            get{ return _updatedDate; }
+        }
+
+        // Constructor field
+        // Creating empty post, considered editing is called later
+        public Post(string title)
+        {
+            _voteNumber = 0;
+            _title = title;
+            creationDate = DateTime.Now;
+            _updatedDate = DateTime.Now;
+        }
+
+        // Creating a post with description together.
+        public Post(string title, string description)
+            :this(title)
+        {
+            _description = description;
+        }
+
+        // Editing and accessing fields
+        // Read the post
+        public string ReadPost()
+        {
+            return _description;
+        }
+
+        // Title can be readed, but not modified.
+        public string GetTitle()
+        {
+            return _title;
+        }
+
+        // Option for later editing
+        public void EditPost(string description)
+        {
+            _description = description;
+            _updatedDate = DateTime.Now;
+        }
+
+        // Upvote
+        public void UpVote()
+        {
+            _voteNumber++;
+        }
+
+        // Downvote
+        public void DownVote()
+        {
+            _voteNumber--;
+        }
+
+        // Get votes
+        public int GetVotes()
+        {
+            return _voteNumber;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace StackOverflowExcersise
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            //preparing random votes
+            Random RandomGen = new Random();
+            int UpRand = RandomGen.Next(1,15);
+            int DownRand = RandomGen.Next(1, 15);
+
+
+            //creating a post
+            var SofPost1 = new Post("How to write this class?", "I am curious that it is right or not. Idea?");
+            //editing a post
+            SofPost1.EditPost(  "I know this is not a real editing, but can overwrite the text before.\n" +
+                                "With ReadPost() it should cover an editor input/output field");
+                       
+            //rating a post
+            for(int i=0; i<UpRand; i++)
+                SofPost1.UpVote();
+            for(int i=0; i<DownRand; i++)
+                SofPost1.DownVote();
+
+            //get esentials
+            Console.WriteLine("<Post: {0}\t{1}>", SofPost1.GetTitle(), SofPost1.creationDate);
+            Console.WriteLine("<Description:>\n{0}", SofPost1.ReadPost());
+            Console.WriteLine("<Votes: {0}>", SofPost1.GetVotes());
+            Console.WriteLine("<Last Updated: {0}>", SofPost1.UpdateDate);
+            Console.WriteLine("\nFor science: \ndownVotes: {0} \nupVotes: {1}", DownRand, UpRand);
+
+        }
+    }
+}
